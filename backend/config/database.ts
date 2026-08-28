c14f26c97a80a3361b20919fe502e09a060eda19
Riad Hasan <rhmitulbd3@gmail.com>
2026-08-28 22:47:20 +0600

    fix postgres connection fallback

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -34,7 +34,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
     postgres: {
       client: 'postgres',
       connection: {
-        connectionString: env('DATABASE_URL'),
+        connectionString: env('DATABASE_URL', ''),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
